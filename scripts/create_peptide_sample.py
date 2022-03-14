eda2167b6ba4a3c602a243bad7671bb075f4b799
christian.peder@hotmail.com <christian.peder@hotmail.com>
2022-03-14 14:38:33 +0100

    adding datasets

--- scripts/create_peptide_sample.py
+++ scripts/create_peptide_sample.py
@@ -4,5 +4,3 @@ import numpy as np
 data = pd.read_csv("data/filtered_data_IEDB_4_tested_len_9_10_full_HLA.csv")
 sample_allele = data[data["HLA_allele"]=="HLA-A*02:01"]
 print(sample_allele)
-# Creating sample of data
-# allele_data = data[data[""]==]
