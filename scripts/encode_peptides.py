533d64b28621197744ee114f1a7c18a3ab240ce4
christian.peder@hotmail.com <christian.peder@hotmail.com>
2022-03-17 09:54:09 +0100

    Adding peptide encodings

---
+++ scripts/encode_peptides.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+def encode_peptide_AAindex(peptide):
+    """Encode peptides using principal components derived from the AAindex database describing ~90% variance of all features derived from the AAindex database
+
+    Args:
+        peptide (str): peptide to encode
+
+    Returns:
+        np.array: encoded peptides
+    """
+    PCA_repr = pd.read_csv("data/PCA_repr_aa.csv",index_col=0)
+    encoded_peptide = []
+    for aa in list(peptide):
+        if aa == "-":
+            n_pcs = len(PCA_repr.columns)
+            encoded_peptide.append([0 for _ in range(n_pcs)])
+        else:
+            encoded_peptide.append(list(PCA_repr.loc[aa]))
+    return np.array(encoded_peptide)
+    ...
+
+
+# TODO: Use other encoding schemes like sparse onehot encoding or BLOSSUM
+def encode_peptide_onehot(peptide):
+    amino_acids = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F','P', 'S', 'T', 'W', 'Y', 'V']
+    ...
+
+
+if __name__ == "__main__":
+    os.system("ls ../")
+    encode_peptide_AAindex("AAVKAGAAL-")
