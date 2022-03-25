a2d5bc858111164e56e59ab439107881cc07eae7
christian.peder@hotmail.com <christian.peder@hotmail.com>
2022-03-25 14:04:14 +0100

    adding correct partitioning and updatingreproduce

--- scripts/format_dataset_for_partitioning.py
+++ scripts/format_dataset_for_partitioning.py
@@ -3,6 +3,6 @@ import pandas as pd
 import sys
 import numpy as np
 
-dataset = pd.read_csv("../data/filtered_data_IEDB_4_tested_len_9_10_full_HLA_IFNg_assay.csv")
+dataset = pd.read_csv("data/filtered_data_IEDB_4_tested_len_9_10_full_HLA_Multi_assay.csv")
 dataset["target"] = [1 if "Positive" in x else 0 for x in dataset["Response"]]
-dataset[["peptide","target","HLA_allele"]].to_csv("../data/partioning_format_filtered_data_IEDB_4_tested_len_9_10_full_HLA_IFNg_assay.tsv",sep="\t",index=False)
+dataset[["peptide","target","HLA_allele"]].to_csv("data/partioning_format_filtered_data_IEDB_4_tested_len_9_10_full_HLA_Multi_assay.tsv",sep="\t",index=False)
