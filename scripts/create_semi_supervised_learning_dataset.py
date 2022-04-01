20500f8b5e90f1386fdb82ad81e5ea62611b3418
christian.peder@hotmail.com <christian.peder@hotmail.com>
2022-04-01 08:13:11 +0200

    creating semi-supervised learning data

--- scripts/create_semi_supervised_learning_dataset.py
+++ scripts/create_semi_supervised_learning_dataset.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 # %%
 def plot_barplot(feature,xlabel):
     unique_entries, counts = np.unique(feature,return_counts=True)
@@ -26,6 +27,32 @@ def hla_df_to_dic(hla):
         col2 = hla['pseudo'].iloc[i]  # pseudo sequence
         dic[col1] = col2
     return dic
+
+def calculate_identity(query,database):
+    if len(query) == len(database):
+        return np.sum([1 for i in range(len(query)) if query[i] == database[i]])/len(query)
+    
+    elif len(query) > len(database):
+        max_score = 0
+        diff_len = len(query) - len(database)
+        for i in range(diff_len + 1):
+            query_shortened = query[i:i+len(database)]
+            score = np.sum([1 for i in range(len(query_shortened)) if query_shortened[i] == database[i]])/len(query_shortened)
+            if score > max_score:
+                max_score = score
+
+        return max_score
+    
+    elif len(query) < len(database):
+        max_score = 0
+        diff_len = len(database) - len(query)
+        for i in range(diff_len + 1):
+            database_shortened = database[i:i+len(query)]
+            score = np.sum([1 for i in range(len(query)) if query[i] == database_shortened[i]])/len(database_shortened)
+            if score > max_score:
+                max_score = score
+
+        return max_score
 # %%
 path_to_epitope_file = "../data/MHCI_binding/epitope_table_export_1647521521.csv"
 epitope_raw_df =  pd.read_csv(path_to_epitope_file,skiprows=1,low_memory=False)
@@ -49,6 +76,7 @@ epitope_df = epitope_raw_df[epitope_raw_df.epitope_length.isin(filter_lengths)]
 epitope_IDS = epitope_df["Epitope ID"]
 final_dataset = dict()
 # %%
+print("## Creating dataframe from raw data ")
 for ID in epitope_IDS:
     if len(experiment_raw_df[experiment_raw_df["Epitope ID"] == ID]) == 0:
         continue
@@ -74,7 +102,6 @@ for ID in epitope_IDS:
             if assay not in final_dataset[unique_identifier].keys():
                 final_dataset[unique_identifier]["Qualitative Measure"] = qualitative_measure
 
-
 # %%
 dataset = []
 for peptide_HLA in final_dataset.keys():
@@ -83,18 +110,33 @@ for peptide_HLA in final_dataset.keys():
     dataset.append([peptide,HLA_allele,qualitive_measure])
 
 columns = ["peptide","HLA_allele","Qualitative Measure"]
-dataset_df = pd.DataFrame.from_records(dataset,columns=columns)
-alleles = ["*" in x for x in dataset_df["HLA_allele"]]
-dataset_df = dataset_df[alleles]
-# %%
-dataset_df
+unsupervised_df = pd.DataFrame.from_records(dataset,columns=columns)
+alleles = ["*" in x for x in unsupervised_df["HLA_allele"]]
+unsupervised_df = unsupervised_df[alleles]
 # %%
 hla = pd.read_csv('../data/formatted_MHC_pseudo.dat', sep=" ")
 hla_dic = hla_df_to_dic(hla)
-removed_idx = dataset_df[dataset_df.apply(lambda x: False if (x.HLA_allele) in hla_dic.keys() else True,axis=1)].index
+removed_idx = unsupervised_df[unsupervised_df.apply(lambda x: False if (x.HLA_allele) in hla_dic.keys() else True,axis=1)].index
 print("These rows were removed due to HLA missing from the MHC_psuedo.dat: {}".format(removed_idx))
-dataset_df = dataset_df[dataset_df.apply(lambda x: True if (x.HLA_allele) in hla_dic.keys() else False,axis=1)]
-dataset_df.to_csv("../data/semi_supervised_data.csv")
-# %%
+unsupervised_df = unsupervised_df[unsupervised_df.apply(lambda x: True if (x.HLA_allele) in hla_dic.keys() else False,axis=1)]
 
+# %%
+path_to_dataset = "../data/filtered_data_IEDB_4_tested_len_9_10_full_HLA_Multi_assay_w_binding.csv"
+multi_assay_df = pd.read_csv(path_to_dataset)
+
+# %%
+print("## Checking for overlap to immunogenecity data")
+black_list_indexes = []
+for i,unsupervised_row in tqdm(unsupervised_df.iterrows()):
+    uns_peptide,uns_HLA = unsupervised_row['peptide'], unsupervised_row['HLA_allele']
+    for j,imm_row in multi_assay_df.iterrows():
+        imm_peptide, imm_hla = imm_row['peptide'], imm_row['HLA_allele']
+        identitiy_score = calculate_identity(uns_peptide,imm_peptide)
+        if identitiy_score == 1.0 and uns_HLA == imm_hla:
+            black_list_indexes.append(i)
+        
+
+# %%
+unsupervised_df = unsupervised_df.drop(unsupervised_df.index[black_list_indexes])
+unsupervised_df.to_csv("../data/semi_supervised_data_w_binding_no_overlap.csv")
 # %%
